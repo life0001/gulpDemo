8c2484d594000f42d20a74abb99262ca97ea1a57
Patrick <life0001@126.com>
2016-10-18 18:12:40 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 var gulp = require('gulp'),
-	imagemin = require('gulp-imagemin'),
-	livereload = require('gulp-livereload'),
-	del = require('del');
+	concat = require('gulp-concat'),
+	cssmin = require('gulp-minify-css'),
+	jsmin = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin');
 // 图片压缩
 gulp.task('images', function () {
 	return gulp.src('img/*')
 		.pipe(imagemin({optimizationLevel: 3, progressive: true, interlaced: true}))
 		.pipe(gulp.dest('dest/img'));
 });
+// css合并压缩
+gulp.task('cssmin', function () {
+	return gulp.src('uzindex.css')
+			.pipe(concat('uzindexMin.css'))
+			.pipe(cssmin())
+			.pipe(gulp.dest('dest/'));
+});
+
 // clean
 gulp.task('Clean', function (cb) {
 	del(['dest/img'], cb)
 });
-
 // livereload
 gulp.task('watch', function () {
 	livereload.listen();
